505e22e34259264782a936b587c5af8ec8eb396c
LastLoper <ys.mobility@gmail.com>
2022-11-21 21:08:15 +0900

    first commit

---
+++ RecommendCreditCard/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecommendCreditCard
+//
+//  Created by Walter J on 2022/11/21.
+//
+
+import UIKit
+
+class MainVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RecommendCreditCard/View/CreditCardListCell.swift
@@ -0,0 +1,23 @@
+//
+//  CreditCardListCell.swift
+//  RecommendCreditCard
+//
+//  Created by Walter J on 2022/11/21.
+//
+
+import UIKit
+
+class CreditCardListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
